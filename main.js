fa4353c4aac1921174e829dd2694f9caae2c67cb
brilliantsunlight1109 <brilliantsunlight1109@gmail.com>
2024-02-07 16:25:26 +0900

    first commit

---
+++ main.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const port = 5002;
+console.log("port: ", port);
+
+const app = express();
+
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
